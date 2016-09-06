3a76e266ad0306224653a690fcd16e9f66906451
leico <yoshitaka_12@iamas.ac.jp>
2016-09-07 05:45:50 +0900

    Try Fbo

--- 013_FixBlink/src/tkm001.h
+++ 013_FixBlink/src/tkm001.h
@@ -15,131 +15,102 @@
 
 
 class tkm001 :public Base{
-    
-private:
+
+  private:
     ofPoint pos[5];
     int rag;
     int hueNum;
     int frameNum;
     int frameNumA;
     vector < ofPoint > humans; //vector 可変長配列　超便利配列！
-    
-    
-    
-public:
-    
-    
-    
-    
-    
+
+    ofFbo fbo;
+
+  public:
     void setup(void){
-        
-        rag=0;
-        hueNum=0;
-        ofSetBackgroundAuto(false);
-        
-        ofClear(0);
-      glFlush();
-      ofClear(0);
+
+      rag=0;
+      hueNum=0;
+      ofSetBackgroundAuto(false);
+      ofFill();
+
+      fbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGB);
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
     void osc(vector<ofxOscMessage> &m){
-        
-        /*
-         
-         int size = (m.size() > 5) ? 5 : m.size();
-         
-         for(int i = 0 ; i < size ; i++){
-         if(m[i].getAddress()=="/human"){
-         m[i].getArgAsInt32(0);//X
-         m[i].getArgAsInt32(1);//Y
-         m[i].getArgAsInt32(2);//Z
-         
-         pos[i].set(m[i].getArgAsInt32(0),m[i].getArgAsInt32(1),m[i].getArgAsInt32(2));
-         
-         }*/
-        
-        
-        humans.resize(m.size());// m.size()でvectorの大きさ、resizeでサイズ変更
-        int j = 0;
-        for(int i = 0 ; i < m.size() ; ++i ){
-            if(m[i].getAddress()== "/human"){
-                
-                humans[j].x = m[i].getArgAsInt32(0);
-                humans[j].y = m[i].getArgAsInt32(1);
-                humans[j].z = m[i].getArgAsInt32(2);
-                
-                ++j;
-            }
+
+      humans.resize(m.size());// m.size()でvectorの大きさ、resizeでサイズ変更
+      int j = 0;
+      for(int i = 0 ; i < m.size() ; ++i ){
+        if(m[i].getAddress()== "/human"){
+
+          humans[j].x = m[i].getArgAsInt32(0);
+          humans[j].y = m[i].getArgAsInt32(1);
+          humans[j].z = m[i].getArgAsInt32(2);
+
+          ++j;
         }
+      }
     }
 
-    
-  
-    
-    
+
+
+
+
     void update(void){
-        
-        
-        int waru = frameNum % 6;
-        
-        if(waru == 0){
-            hueNum++;
-        }
-        if(hueNum >= 255){
-            hueNum=0 ;
-        }
-        frameNum ++;
-        frameNumA ++;
-        
-    }
-    
 
-    
-    
-    
-    
+
+      int waru = frameNum % 6;
+
+      if(waru == 0){
+        hueNum++;
+      }
+      if(hueNum >= 255){
+        hueNum=0 ;
+      }
+      frameNum ++;
+      frameNumA ++;
+
+    }
+
+
+
+
+
+
     void draw(void){
-        
-        
-        
+
+
+      fbo.begin();
         ofColor c;
         c.setHsb(hueNum, 255, 255,200);
         ofSetColor( c );
-        
-        
-        
-       for(int i = 0 ;i < humans.size() ;i ++){
-           ofPushMatrix();
-        
-           ofTranslate(humans[i].x,humans[i].y,humans[i].z);
-           int z = humans[i].z;
-           
-           ofFill();
-        
-           ofDrawCircle(0, 0, z/6);
-           ofPopMatrix();
 
-           
+        for(int i = 0 ;i < humans.size() ;i ++){
+          ofPushMatrix();
+
+            ofTranslate(humans[i].x,humans[i].y,humans[i].z);
+            int z = humans[i].z;
+
+            ofDrawCircle(0, 0, z/6);
+          ofPopMatrix();
         }
+
         if( frameNum >=6000 ){
-        ofSetColor(0,0,0,10);
-        ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
-            
-            if(frameNum==6080)frameNum=0;
-            
-            }
-        
+          ofSetColor(0,0,0,10);
+          ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
+
+          if(frameNum==6080)frameNum=0;
+        }
+      fbo.end();
+
+      fbo.draw(0, 0);
+
     }
-   
-    
+
+
 
 
 
