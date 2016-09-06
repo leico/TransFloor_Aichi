1a75ae951c3ec427de39fc83b0c8385fd945248e
leico <yoshitaka_12@iamas.ac.jp>
2016-09-07 01:53:05 +0900

    Test Timer

---
+++ 011_TimerTest/src/MainScreen.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+#include "ofxJSON.h"
+#include "ofxSimpleTimer.h"
+#include "KinectCV.h"
+#include "Base.h"
+#include "tkm001.h"
+#include "Blank.h"
+#include "RippleScene.h"
+#include "Relation.h"
+#include "Gravity.h"
+
+class MainScreen : public ofBaseApp{
+
+  ofxOscReceiver receiver;
+
+  const static std :: string jsonfile;
+
+  const static std :: string kinect1_prefix;
+               KinectCV      kinect1;
+  const static std :: string kinect2_prefix;
+               KinectCV      kinect2;
+
+  ofxSimpleTimer<std :: string> nohuman;
+  void SceneChange(std :: string& id);
+
+        bool b_debug;
+  const bool Debug(void);
+        void Debug(const bool debug);
+
+
+  const static int SCENE_NUM = 4;
+  int  scene;
+  Base *basedraw;
+  void DisplaySetup(void);
+
+  public:
+  void setup();
+  void exit();
+  void update();
+  void draw();
+
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y);
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
+
+
+};
+
+
+inline const bool MainScreen :: Debug(void)            { return b_debug;  }
+inline       void MainScreen :: Debug(const bool debug){ b_debug = debug; }
+
+inline void MainScreen :: DisplaySetup(void){
+
+  ofSetFrameRate(60);
+  ofSetVerticalSync(true);
+
+  ofBackground(0);
+
+  ofSetBackgroundAuto(true);
+
+  ofFill();
+  ofSetLineWidth(1);
+  ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+
+}
+
